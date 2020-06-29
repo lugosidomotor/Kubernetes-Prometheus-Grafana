b3111523e30b00459a5c40166d84684b02985632
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-06-29 20:35:16 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,22 +13,24 @@ sudo apt-get install conntrack -y
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
+#KUBECTL INSTALL
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
 
+#HELM INSTALL
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 sudo ./get_helm.sh
 sudo helm init
 
 sudo minikube addons enable ingress
+sudo kubectl patch svc ill-bat-prometheus-server  -p '{"spec": {"type": "NodePort"}}'
 
 sudo minikube start --vm-driver=none
 sudo helm install stable/prometheus
-
 
 
 #sudo minikube start –vm-driver=none –extra-config=kubeadm.ignore-preflight-errors=NumCPU –force –cpus 1
