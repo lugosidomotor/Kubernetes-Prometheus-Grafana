039e957b984626aadbcbe0aa4b7968513eda62ed
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-06-29 14:31:54 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+#Reason of the double update: https://github.com/hashicorp/terraform/issues/1025
+sudo apt-get update -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y docker.io
+sudo apt-get install socat -y
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.18.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
