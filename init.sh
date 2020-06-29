b918bffa86a2bb3dbe40ca90e9a9ed2963abe155
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-06-29 20:18:38 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,7 +13,11 @@ sudo apt-get install conntrack -y
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-sudo apt-get install kubectl -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
