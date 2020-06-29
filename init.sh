6996b966bebfe38be85e5009db860ad1eeedf8da
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-06-29 15:39:01 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,13 +10,18 @@ sudo apt-get install -y docker.io
 sudo apt-get install socat -y
 sudo apt-get install conntrack -y
 
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+sudo apt-get install kubectl -y
+
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 sudo ./get_helm.sh
 sudo helm init
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.18.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 sudo minikube start --vm-driver=none
+sudo helm install stable/prometheus
 
-sudo minikube start –vm-driver=none –extra-config=kubeadm.ignore-preflight-errors=NumCPU –force –cpus 1
+#sudo minikube start –vm-driver=none –extra-config=kubeadm.ignore-preflight-errors=NumCPU –force –cpus 1
